12fa5e1927545ac47c9c67daf3e5abad7fcffa9f
writtenriddle <152046781+writtenriddle@users.noreply.github.com>
2024-03-23 11:02:42 +0530

    Add files via upload

--- clicker.ps1
+++ clicker.ps1
@@ -1,7 +1,24 @@
+Add-Type -TypeDefinition @"
+    using System;
+    using System.Runtime.InteropServices;
+
+    public class MouseInput {
+        [DllImport("user32.dll",CharSet=CharSet.Auto, CallingConvention=CallingConvention.StdCall)]
+        public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        public static void ClickLeftMouseButton() {
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        }
+    }
+"@
+
 $delay = 500
 $i = 0
 while($i -eq 0)
 {
-Click-MouseButton left
-Start-Sleep -m $delay
+    [MouseInput]::ClickLeftMouseButton()
+    Start-Sleep -Milliseconds $delay
 }
